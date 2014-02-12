6da19004d73e5e09418f84d08c6cc2ea87118026
the-godfather <radek1236987@gmail.com>
2014-02-13 00:19:27 +0100

    get_playlist.py modified: do not strip youtube api urls added example of usage with mplayer2

--- youtube/get_playlist.py
+++ youtube/get_playlist.py
@@ -6,15 +6,17 @@ import re
 if len (sys.argv) < 2:
     print "Usage:"
     print sys.argv[0] + " [youtube playlist link]"
+    print "Example of usage:"
+    print sys.argv[0] + " [youtube playlist link]" " | mplayer2 -playlist -"
     sys.exit(1)
 
 link = sys.argv[1]
 id = re.findall(".*list=(.*)",link)[0]
-
 request_api_url = "https://gdata.youtube.com/feeds/api/playlists/{0}?v=2&alt=json"
+
 try:
     data = requests.get(request_api_url.format(id)).json()
     for entry in data['feed']['entry']:
-        print entry['link'][0]['href'].strip("&feature=youtube_gdata")
+        print entry['link'][0]['href']
 except:
     pass
